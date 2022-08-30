7052d2b09112bb8c01cf17672e4115ab7fda33ea
Federico <fedeaacevedo@gmail.com>
2022-08-30 17:26:29 -0300

    Routes

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express = require('express');
+import express from 'express';
+import usuarioRouter from './usuarioRoutes.js'
+
 
 //creamos la app
 const app = express();
 
+
+//Habilitar PUG
+app.set('view engine', 'pug')
+app.set('views', './views')
+
 //Routing
-app.get('/', function(req, res){
-    res.send("Hola mundo en express")
-})
+app.use('/auth',usuarioRouter)
 
 
 //Definir un puerto
 const port = 3000;
-
 app.listen(port, () => {
     console.log(`El puerto en uso es ${port}`)
 })
