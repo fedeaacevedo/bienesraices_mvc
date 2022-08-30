eae575960a47dafff42f94d6e408ae4c0901d782
Federico <fedeaacevedo@gmail.com>
2022-08-30 17:21:23 -0300

    Rutas

---
+++ usuarioRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const router = express.Router();
+
+router.get('/login', (req, res) => {
+    res.render('auth/login',{
+        autenticado: true,
+        
+    })
+})
+
+export default router
